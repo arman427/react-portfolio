22460b09af1982a36d45f9418d5a09cab1efa295
Arman <babayananuta11@gmail.com>
2025-12-13 15:39:13 +0300

    About me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,33 @@
+import aboutMe from './../images/about-me.jpg'
+import AboutSocials from './AboutSocials';
+
 const About = () => {
    return (
       <>
          <section className="about mt-150">
             <div className="container mx-auto">
-               <div className="about-content">
-                  <h1 className="md:text-7xl text-3xl lg:text-8xl font-bold text-center text-white relative z-20 ">Обо мне</h1>
-                  <p className="mx-auto w-180 text-center text-white/70 mt-10">Я начинающий фронтенд-разработчик с примерно одного года целенаправленного обучения и практики. Мой путь в веб-разработку начался с искреннего любопытства, которое быстро переросло в настоящую страсть.</p>
+               <div className="about-content flex items-center justify-between gap-10">
+                  <div className="about-img__wrapper rounded-3xl">
+                     <img src={aboutMe} alt="" className='rounded-3xl' />
+                  </div>
 
+                  <div className="about-desc w-[1000px]">
+                     <h1 className="md:text-7xl text-3xl lg:text-8xl font-bold text-white relative z-20 mb-2">Обо <span className="gradient-text">мне</span></h1>
+                     <p className="text-white/70 mb-5">Я начинающий фронтенд-разработчик с примерно одного года целенаправленного обучения и практики. Мой путь в веб-разработку начался с искреннего любопытства, которое быстро переросло в настоящую страсть.</p>
+
+                     <ul className='text-white/70 grid gap-1'>
+                        <li><span className='grad-linii'>Навыки:</span> Уверенно работаю с HTML5, CSS3 (включая Flexbox/Grid) и JavaScript (ES6+). Сейчас активно углубляюсь в Tailwind и React и осваиваю работу с системами контроля версий, такими как Git.
+                        </li>
+                        <li>
+                           <span className='grad-linii'>Опыт:</span> За этот год я разработал около 10 учебных и пет-проектов, включая адаптивные лендинги, небольшие приложения с API-интеграцией.
+                        </li>
+                        <li>
+                           <span className='grad-linii'>Моя цель:</span> Создавать чистый, элегантный и, самое главное, удобный для пользователя код, который приносит реальную пользу. Я постоянно учусь и ищу возможности применить свои знания в реальных проектах.
+                        </li>
+                     </ul>
+
+                     <AboutSocials />
+                  </div>
                </div>
             </div>
          </section>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
          <div className="header-nav mt-10 px-15 py-3 rounded-4xl relative border border-white/30 group duration-200 hover:border-white/50 overflow-hidden" ref={buttonRef} onMouseEnter={buttonEnter} onMouseLeave={buttonLeave} onMouseMove={buttonMove}>
             <span className="header-animation" style={animationStyle} />
             <div className="absolute w-full h-full inset-0 bg-white/30 rounded-3xl blur-xl opacity-20 -z-10 duration-200 ease group-hover:opacity-30" />
-            <ul className="header-nav__list flex gap-15 text-white/70">
+            <ul className="header-nav__list flex gap-10 text-white/70">
                <li><a href="" className="hover:text-white duration-200 ease">Главная</a></li>
                <li><a href="" className="hover:text-white duration-200 ease">Обо мне</a></li>
                <li><a href="" className="hover:text-white duration-200 ease">Проекты</a></li>
